32e06967cefd32db9eda09cfb14c246ad05ba954
Jenom <119628596+shehuj@users.noreply.github.com>
2025-10-29 21:04:12 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,6 +14,14 @@ resource "aws_s3_bucket_versioning" "versioning" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "block" {
+  bucket                  = aws_s3_bucket.multilingua_bucket.id
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = false   # <-- allow your custom policy
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_policy" "policy" {
   bucket = aws_s3_bucket.multilingua_bucket.id
   policy = data.aws_iam_policy_document.bucket_policy.json
