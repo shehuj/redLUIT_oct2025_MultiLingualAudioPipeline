fb765d8025a8b44e6b83f2b163d8cab5fa62ebde
Jenom <119628596+shehuj@users.noreply.github.com>
2025-10-29 20:21:17 -0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,8 @@
-variable "bucket_name" { type = string }
+variable "bucket_name" { 
+    description = "value for the S3 bucket name"
+    type = string
+    default = "multilingua_pipeline_bucket"
+    }
 variable "aws_region"  { 
     type = string 
     default = "us-east-1" 
